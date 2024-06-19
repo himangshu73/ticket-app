921d744c8d26040b010bc19a10daffa4b73d8251
himangshu <himangshu73@gmail.com>
2024-06-19 22:51:26 +0600

    second commit

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,8 @@
+const StatusDisplay = () => {
+  return (
+    <span className="inline-block text-xs font-semibold rounded-full px-2 py-1 text-gray-500 bg-green-300">
+      Done
+    </span>
+  );
+};
+export default StatusDisplay;
